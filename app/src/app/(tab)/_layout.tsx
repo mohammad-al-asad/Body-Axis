e9e03444133300800f0a69373a9e5713a0ab1487
mohammad-al-asad <maasad11914@gmail.com>
2026-05-17 16:47:29 +0600

    feat: project setup

---
+++ app/src/app/(tab)/_layout.tsx
@@ -0,0 +1,14 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import React from "react";
+import { useColorScheme } from "react-native";
+
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+
+export default function TabLayout() {
+  return <AppTabs />;
+}
